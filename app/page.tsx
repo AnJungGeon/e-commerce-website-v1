39dd8659922e806e478b8eff26aad617cf0d71e9
AnJungGeon <gunn0128@gmail.com>
2026-01-11 20:45:24 +0900

    feat: 홈 페이지에 상품 섹션 추가
    - ProductCard 컴포넌트를 사용한 제품 섹션 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-import { categoryItems } from "@/data/data";
+import {categoryItems, allProducts } from "@/data/data";
 import Image from "next/image";
+import ProductCard from "@/components/ProductCard";
+import Link from "next/link";
 
 export default function Home(){
   return <>
@@ -12,7 +14,7 @@ export default function Home(){
     text-center lg:rounded-2xl">
       <p className="text-amber-400 font-light tracking-wide
       uppercase">
-        Minimal interiror desing
+        Minimal interior design
       </p>
       <h1 className="text-4xl md:text-5xl lg:text-6xl">
         We minimize your waste <br/> 
@@ -22,28 +24,50 @@ export default function Home(){
   </section>
 
   {/*Category section*/}
-  <section className="">
-    <div className="container">
+  <section className="mt-16 relative z-10 lg:mt-36">
+    <div className="container grid gap-7 sm:grid-cols-2
+    lg:grid-cols-3">
       {categoryItems.map(item =>(
         //Card
         <div className="bg-white border border-amber-100 block hover:bg-amber-50
-        rounded-xl px-10 py-8" key={item.id}>
+        rounded-xl px-10 py-8 transition
+        cursor-pointer" key={item.id}>
           {/* Titel and quantity */}
           <div>
-            <h2 className="">{item.title}</h2>
-            <p className="">{item.quantity}</p>
+            <h2 className="text-2xl">{item.title}</h2>
+            <p className="text-gray-500">{item.quantity}</p>
           </div>
           {/* Product image */}
-          <div className="">
+          <div className="max-w-max mx-auto mt-12">
             <Image 
             src={item.img} 
             alt={item.title}
             width={item.width}
             height={item.height}
+            className="w-auto h-auto object-contain"
             />
           </div>
         </div>
       ))}
+    </div>
+  </section>
+
+  {/* Products */}
+  <section className="pt-28">
+    <div className="container">
+      {/* title */}
+      <h2 className="section-title text-center">Explore All Products</h2>
+      {/* card wrapper */}
+      <div className="mt-14 grid gap-5 sm:grid-cols-2
+      lg:grid-cols-3">
+        {allProducts.slice(4, 12).map(product => (
+          <ProductCard key={product.id} {...product}/>
+        ))}
+      </div>
+
+      {/* Btn */}
+      <Link href='/shop' className="btn-primary block mt-14
+      mx-auto max-w-max">View all Products</Link>
     </div>
   </section>
   </>
